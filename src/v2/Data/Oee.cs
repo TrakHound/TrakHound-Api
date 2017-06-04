5cd1c27b51f0c2ec5fd35224c31dd69f03cf1115
Patrick Ritchie <Patrick@TrakHound.com>
2017-06-04 16:26:03 -0400

    Added Oee classes and requests

---
+++ src/v2/Data/Oee.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) 2017 TrakHound Inc., All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace TrakHound.Api.v2.Data
+{
+    public class Oee
+    {
+        [JsonProperty("oee")]
+        public double Value { get; set; }
+
+        [JsonProperty("from")]
+        public DateTime From { get; set; }
+
+        [JsonProperty("to")]
+        public DateTime To { get; set; }
+
+        [JsonProperty("availability")]
+        public Availability Availability { get; set; }
+
+        [JsonProperty("performance")]
+        public Performance Performance { get; set; }
+
+        [JsonProperty("quality")]
+        public Quality Quality { get; set; }
+    }
+
+    public class Availability
+    {
+        [JsonProperty("operating_time")]
+        public double OperatingTime { get; set; }
+
+        [JsonProperty("planned_production_time")]
+        public double PlannedProductionTime { get; set; }
+
+        [JsonProperty("value")]
+        public double Value { get; set; }
+
+        [JsonProperty("events")]
+        public List<AvailabilityEvent> Events { get; set; }
+    }
+
+    public class AvailabilityEvent
+    {
+        [JsonProperty("event")]
+        public string Event { get; set; }
+
+        [JsonProperty("start")]
+        public DateTime Start { get; set; }
+
+        [JsonProperty("stop")]
+        public DateTime Stop { get; set; }
+
+        [JsonProperty("duration")]
+        public double Duration { get; set; }
+    }
+
+    public class Performance
+    {
+        [JsonProperty("operating_time")]
+        public double OperatingTime { get; set; }
+
+        [JsonProperty("ideal_operating_time")]
+        public double IdealOperatingTime { get; set; }
+
+        [JsonProperty("value")]
+        public double Value { get; set; }
+
+        [JsonProperty("events")]
+        public List<PerformanceEvent> Events { get; set; }
+    }
+
+    public class PerformanceEvent
+    {
+        [JsonProperty("feedrate_override")]
+        public double FeedrateOverride { get; set; }
+
+        [JsonProperty("start")]
+        public DateTime Start { get; set; }
+
+        [JsonProperty("stop")]
+        public DateTime Stop { get; set; }
+
+        [JsonProperty("total_time")]
+        public double TotalTime { get; set; }
+
+        [JsonProperty("operating_time")]
+        public double OperatingTime { get; set; }
+
+        [JsonProperty("ideal_operating_time")]
+        public double IdealOperatingTime { get; set; }
+    }
+
+    public class Quality
+    {
+        [JsonProperty("total_parts")]
+        public int TotalParts { get; set; }
+
+        [JsonProperty("good_parts")]
+        public int GoodParts { get; set; }
+
+        [JsonProperty("value")]
+        public double Value { get; set; }
+    }
+}
---
+++ src/v2/Requests/Oee.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) 2017 TrakHound Inc., All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace TrakHound.Api.v2.Requests
+{
+    public class Oee
+    {
+        /// <summary>
+        /// Request OEE Data for a single device
+        /// </summary>
+        public static List<Data.Oee> Get(string baseUrl, string deviceId, DateTime from, DateTime to, int increment, string accessToken)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(deviceId + "/oee", Method.GET);
+            if (from > DateTime.MinValue) request.AddQueryParameter("from", from.ToString("o"));
+            if (to > DateTime.MinValue) request.AddQueryParameter("to", to.ToString("o"));
+            if (increment > 0) request.AddQueryParameter("increment", increment.ToString());
+            if (!string.IsNullOrEmpty(accessToken)) request.AddQueryParameter("access_token", accessToken);
+
+            var response = client.Execute(request);
+            if (response != null && response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var json = response.Content;
+                if (!string.IsNullOrEmpty(json))
+                {
+                    var obj = Json.Convert.FromJson<List<Data.Oee>>(json);
+                    if (obj != null) return obj;
+                }
+            }
+
+            return null;
+        }
+
+        #region "Overloads"
+
+        public static List<Data.Oee> Get(string baseUrl, string deviceId, DateTime from, string accessToken = null)
+        {
+            return Get(baseUrl, deviceId, from, DateTime.MinValue, 0, accessToken);
+        }
+
+        public static List<Data.Oee> Get(string baseUrl, string deviceId, DateTime from, DateTime to, string accessToken = null)
+        {
+            return Get(baseUrl, deviceId, from, to, 0, accessToken);
+        }
+
+        #endregion
+
+
+        public class Stream
+        {
+            private Requests.Stream stream;
+
+            public string BaseUrl { get; set; }
+            public string DeviceId { get; set; }
+            public DateTime From { get; set; }
+            public DateTime To { get; set; }
+            public int Interval { get; set; }
+            public string AccessToken { get; set; }
+
+            public delegate void OeeHandler(List<Data.Oee> oee);
+            public event OeeHandler OeeReceived;
+
+            public Stream(string baseUrl, string deviceId, DateTime from, DateTime to, int interval, string accessToken = null)
+            {
+                BaseUrl = baseUrl;
+                DeviceId = deviceId;
+                From = from;
+                To = to;
+                Interval = interval;
+                AccessToken = accessToken;
+            }
+
+            public void Start()
+            {
+                var uri = new Uri(BaseUrl);
+                var query = "?interval=" + Interval;
+                if (!string.IsNullOrEmpty(AccessToken)) query += "&access_token=" + AccessToken;
+                if (From > DateTime.MinValue) query += "&from=" + From.ToString("o");
+                if (To > DateTime.MinValue) query += "&to=" + To.ToString("o");
+                uri = new Uri(uri, DeviceId + "/oee" + query);
+
+                stream = new Requests.Stream(uri.ToString(), "[", "\r\n");
+                stream.GroupReceived += Stream_GroupReceived;
+                stream.Start();
+            }
+
+            public void Stop()
+            {
+                if (stream != null) stream.Stop();
+            }
+
+            private void Stream_GroupReceived(string json)
+            {
+                var obj = Json.Convert.FromJson<List<Data.Oee>>(json);
+                if (obj != null)
+                {
+                    OeeReceived?.Invoke(obj);
+                }
+            }
+        }
+    }
+}
