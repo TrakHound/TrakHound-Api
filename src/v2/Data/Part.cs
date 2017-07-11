9480148c71d5ef83fc6853cfe52771c3389895b7
Patrick Ritchie <Patrick@TrakHound.com>
2017-07-10 21:41:07 -0400

    Added Parts Api classes

---
+++ src/v2/Data/Part.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2017 TrakHound Inc., All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using Newtonsoft.Json;
+using System;
+
+namespace TrakHound.Api.v2.Data
+{
+    public class Part
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("program_name")]
+        public string ProgramName { get; set; }
+
+        [JsonProperty("start")]
+        public DateTime Start { get; set; }
+
+        [JsonProperty("stop")]
+        public DateTime Stop { get; set; }
+
+        [JsonProperty("duration")]
+        public TimeSpan Duration { get; set; }
+    }
+}
---
+++ src/v2/Requests/Parts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+// Copyright (c) 2017 TrakHound Inc., All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using RestSharp;
+using System.Collections.Generic;
+
+namespace TrakHound.Api.v2.Requests
+{
+    public static class Parts
+    {
+        /// <summary>
+        /// Request Alarms for a single device
+        /// </summary>
+        public static List<Data.Part> Get(string baseUrl, string deviceId, DateTime from, DateTime to, string accessToken = null)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(deviceId + "/parts", Method.GET);
+            if (from > DateTime.MinValue) request.AddQueryParameter("from", from.ToString("o"));
+            if (to > DateTime.MinValue) request.AddQueryParameter("to", to.ToString("o"));
+            if (!string.IsNullOrEmpty(accessToken)) request.AddQueryParameter("access_token", accessToken);
+
+            var response = client.Execute(request);
+            if (response != null && response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var json = response.Content;
+                if (!string.IsNullOrEmpty(json))
+                {
+                    var obj = Json.Convert.FromJson<List<Data.Part>>(json);
+                    if (obj != null) return obj;
+                }
+            }
+
+            return null;
+        }
+
+        #region "Overloads"
+
+        public static List<Data.Part> Get(string baseUrl, string deviceId, DateTime from)
+        {
+            return Get(baseUrl, deviceId, from, DateTime.MinValue);
+        }
+
+        #endregion
+    }
+}
